3b8485b7fd7b00b4bceba12391fef782f1a892af
Zachary Dowd <zacharyd2796@zohomail.com>
2022-07-11 12:56:51 -0600

    connection to server in place

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+
+    public GameObject menuButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseMenus();
+
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting to Network...";
+
+        PhotonNetwork.ConnectUsingSettings();
+
+
+    }
+
+    void CloseMenus()
+    {
+        loadingScreen.SetActive(false);
+        menuButtons.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+
+        loadingText.text = "Joining Lobby...";
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
