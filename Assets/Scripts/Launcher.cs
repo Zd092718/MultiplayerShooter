482741ac3043b1e06f7e05ab27bc370921588637
Zachary Dowd <zacharyd2796@zohomail.com>
2022-07-11 13:35:16 -0600

    room leaving working

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -105,4 +105,18 @@ public class Launcher : MonoBehaviourPunCallbacks
         CloseMenus();
         menuButtons.SetActive(true);
     }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        CloseMenus();
+        loadingText.text = "Leaving Room...";
+        loadingScreen.SetActive(true);
+    }
+
+    public override void OnLeftRoom()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
 }
